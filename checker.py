748d5cd964d81f26d694c852ebc6d163cfe1bece
maslke <maslke@outlook.com>
2024-02-19 00:43:19 +0800

    feat: support for run eslint check.

--- checker.py
+++ checker.py
@@ -2,23 +2,26 @@ import argparse
 import git
 import subprocess
 
-"""
-调用系统命令
-"""
-
 
 def run(cmd):
+    """
+    调用系统命令
+    :param cmd: 命令参数
+    :return:
+    """
     process = subprocess.run(cmd)
     return_code = process.returncode
     print(return_code)
 
 
-"""
-执行checkstyle检测
-"""
-
-
 def run_checkstyle_check(tool_path, output_path, changed_java_files):
+    """
+        执行checkstyle检测
+        :param tool_path: 工具集根路径
+        :param output_path: 检查结果文件输出路径
+        :param changed_java_files: 执行检查的java源代码文件
+        :return:
+        """
     cmd = [
         'java',
         f'-Dcheckstyle.suppressions.file={tool_path}/checkstyle-8.3/ruleFile/suppressions.xml',
@@ -35,21 +38,28 @@ def run_checkstyle_check(tool_path, output_path, changed_java_files):
     run(cmd)
 
 
-"""
-执行pmd检测
-"""
-
-
 def run_pmd_check(tool_path, output_path, changed_java_files):
-    pass
+    """
+    执行pmd检测
+    :param tool_path: 工具集根路径
+    :param output_path: 检查结果文件输出路径
+    :param changed_java_files: 执行检查的java源代码文件
+    :return:
+    """
+    cmd = [
 
-
-"""
-执行重复代码检测，阈值为20行
-"""
+    ]
+    run(cmd)
 
 
 def run_simian_check(tool_path, output_path, changed_java_files):
+    """
+    执行重复代码检测，阈值为20行
+    :param tool_path: 工具集根路径
+    :param output_path: 检查结果文件输出路径
+    :param changed_java_files: 执行检查的java源代码文件
+    :return:
+    """
     cmd = [
         'java',
         '-jar',
@@ -61,12 +71,13 @@ def run_simian_check(tool_path, output_path, changed_java_files):
     run(cmd)
 
 
-"""
-执行圈复杂度检测，检测阈值为10
-"""
-
-
 def run_lizard_check(output_path, changed_java_files):
+    """
+    执行圈复杂度检测，检测阈值为10
+    :param output_path: 检插结果文件输出路径
+    :param changed_java_files: 执行检查的java源代码文件
+    :return:
+    """
     cmd = [
         'python3',
         '-m',
@@ -82,19 +93,57 @@ def run_lizard_check(output_path, changed_java_files):
     run(cmd)
 
 
-def check(project_path, tool_path, vault_path, output_path):
-    repo = git.Repo(vault_path)
+"""
+执行eslint检测
+"""
+
+
+def run_eslint_check(tool_path, output_path, changed_js_files):
+    """
+    执行eslint检查
+    :param tool_path: 工具集根目录
+    :param output_path: 检查结果文件输出路径
+    :param changed_js_files: 执行检查的js源代码文件
+    :return:
+    """
+    cmd = [
+        '/usr/bin/local/node',
+        '--max-old-space-size=1000',
+        f'{tool_path}/fish-cli/bin/fish.js',
+        'lint',
+        '-dir',
+        ' '.join(changed_js_files),
+        '-noCreateFileLog',
+        '-f',
+        f'xml>{output_path}/eslint_result.xml'
+    ]
+    run(cmd)
+
+
+def check(project_path, tool_path, output_folder):
+    """
+    执行代码规范检查
+    :param project_path: 工程文件路径
+    :param tool_path: 执行检查使用的工具集的路径
+    :param output_folder: 检查结果输出文件的存放文件夹名称。默认为target
+    :return:
+    """
+    repo = git.Repo(project_path)
     latest_commit = repo.head.commit
     changed_files = latest_commit.diff(latest_commit.parents[0])
 
     changed_java_files = list()
-    # 打印变化文件列表
+    changed_js_files = list()
+
     for item in changed_files:
         a_path = item.a_path
         if a_path.endswith('.java'):
             changed_java_files.append(f'{project_path}/{a_path}')
+        elif a_path.endswith('.js'):
+            changed_js_files.append(f'{project_path}/{a_path}')
 
-    full_output_path = f'{project_path}/{output_path}' if output_path else project_path
+    output_folder = 'target' if output_folder is None else output_folder
+    full_output_path = f'{project_path}/{output_folder}'
 
     print('execute checkstyle check')
     run_checkstyle_check(tool_path, full_output_path, changed_java_files)
@@ -102,16 +151,20 @@ def check(project_path, tool_path, vault_path, output_path):
     print('execute lizard check')
     run_lizard_check(full_output_path, changed_java_files)
 
+    print('execute pmd check')
+    run_pmd_check(full_output_path, full_output_path, changed_java_files)
+
+    print('execute eslint check')
+    run_eslint_check(tool_path, full_output_path, changed_js_files)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--project', '-p', required=True, help='path of project directory')
     parser.add_argument('--tool', '-t', required=True, help='path of check tool directory')
-    parser.add_argument('--vault', '-v', required=False, help='path of vault directory, .git included'),
     parser.add_argument('--output', '-o', required=False, help='sub folder name of check result file.')
     args = parser.parse_args()
     tool = args.tool
     project = args.project
     output = args.output
-    vault = project if args.vault is None else args.vault
-    check(project, tool, vault, output)
+    check(project, tool, output)
